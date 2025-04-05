78879792976ebb2c166969c8bcfba11348bd43bb
tirso <tir303@icloud.com>
2025-04-05 14:59:21 +0200

    Initial Commit

---
+++ 3D Test/_D_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_TestApp.swift
+//  3D Test
+//
+//  Created by Tirso López Ausens on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct _D_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
